97b59322c41282ef4aa24199355618b94f69e88d
mitsui <b1614196@planet.kanazawa-it.ac.jp>
2018-11-05 14:13:06 +0900

    add: EditTextの中身の削除

--- cuisson_sousvide_app/app/src/main/java/com/example/mitsui/cuisson_sous_vide_app/MainActivity.java
+++ cuisson_sousvide_app/app/src/main/java/com/example/mitsui/cuisson_sous_vide_app/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,14 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText AddressText = findViewById(R.id.device_address);
 
-        Button sendButton = findViewById(R.id.connect_button);
-        sendButton.setOnClickListener(new View.OnClickListener() {
+        Button connectdButton = findViewById(R.id.connect_button);
+        connectdButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplication(), CommandActivity.class);
                 startActivity(intent);
             }
         });
+
+        Button clearButton = findViewById(R.id.clear_button);
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddressText.getText().clear();
+            }
+        });
+
     }
 }
