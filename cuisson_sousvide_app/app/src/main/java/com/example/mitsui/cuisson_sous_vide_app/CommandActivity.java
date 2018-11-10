c78c58835224c9b75694cea21aed8dd712c9ed96
mitsui <b1614196@planet.kanazawa-it.ac.jp>
2018-11-10 11:00:59 +0900

    add: temp_spinnerに値を挿入

--- cuisson_sousvide_app/app/src/main/java/com/example/mitsui/cuisson_sous_vide_app/CommandActivity.java
+++ cuisson_sousvide_app/app/src/main/java/com/example/mitsui/cuisson_sous_vide_app/CommandActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class CommandActivity extends AppCompatActivity{
 
@@ -15,8 +16,15 @@ public class CommandActivity extends AppCompatActivity{
 
         setContentView(R.layout.activity_command);
 
-        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.temp_spinner);
-        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        for(int i = 30; i < 100; i++){
+            adapter.add(String.valueOf(i));
+        }
+        Spinner temp_spinner = (Spinner) findViewById(R.id.temp_spinner);
+
+        // SpinnerにAdapterを設定
+        temp_spinner.setAdapter(adapter);
 
         Button cookButton = findViewById(R.id.cook_button);
         cookButton.setOnClickListener(new View.OnClickListener() {
