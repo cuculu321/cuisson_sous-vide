153ae6bf93e595a406d68923d958a5f9bbe161e3
mitsui <b1614196@planet.kanazawa-it.ac.jp>
2018-11-11 09:56:54 +0900

    add: 温度の取得

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-cuisson_sous-vide
+# cuisson_sous-vide
+
+## User Arduino Libraries
+* https://github.com/PaulStoffregen/OneWire
+
+* https://github.com/milesburton/Arduino-Temperature-Control-Library
---
+++ temp_check/temp_check.ino
@@ -0,0 +1,18 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+  
+#define temp_pin 36 // データ(黄)で使用するポート番号
+#define SENSER_BIT    9      // 精度の設定bit
+  
+OneWire oneWire(temp_pin);
+DallasTemperature sensors(&oneWire);
+  
+void setup(void) {
+  Serial.begin(9600); //温度表示確認用
+  sensors.setResolution(SENSER_BIT);
+}
+  
+void loop(void) {
+  sensors.requestTemperatures();              // 温度取得要求
+  Serial.println(sensors.getTempCByIndex(0)); //温度の取得&シリアル送信
+}
