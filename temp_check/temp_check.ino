d0b822d248cadf36da6c9113e69777eeea5fb926
mitsui <b1614196@planet.kanazawa-it.ac.jp>
2018-11-17 21:06:15 +0900

    add: ループ内で温度をPub

--- temp_check/temp_check.ino
+++ temp_check/temp_check.ino
@@ -1,6 +1,21 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
-  
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// WiFi
+const char *ssid = "WARPSTAR-9C16EB-W";
+const char *passwd = "9A57BC522C952";
+
+// Pub/Sub
+const char* mqttHost = "192.168.0.8"; // MQTTのIPかホスト名
+const int mqttPort = 1883;       // MQTTのポート
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
+
+const char* topic = "micon/temp";     // 送信先のトピック名
+char payload[10];                   // 送信するデータ
+
 #define temp_pin 22 // データ(黄)で使用するポート番号
 #define SENSER_BIT 9      // 精度の設定bit
 #define HEAT_PIN 32 //SSRにつなぐポート
@@ -18,25 +33,102 @@
 OneWire oneWire(temp_pin);
 DallasTemperature sensors(&oneWire);
 
+//温度関係
 float temp = 0;
 float use_temp = 0; //平均値を求めるに使う変数
 float temp_box[STACK_NUM] = {0};
 float send_temp = 0; //送信データに使用する変数
 int read_count = 0;
-int target = 60;
+int target = 70;
 
+//PI制御関係
 float duty = 0;
 float dt, preTime;
 float vol;
 float P, I, D, preP, pg, ig, pwm;
 
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, passwd);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(1, LOW); 
+    Serial.println("coming here");
+  } else {
+    digitalWrite(1, HIGH); 
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!mqttClient.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP32Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (mqttClient.connect(clientId.c_str(), "kies", "wtpotnt")) {
+      Serial.println("connected");
+      mqttClient.subscribe("#");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void temp_pub(){
+  itoa(send_temp, payload, 10);
+  mqttClient.publish(topic, payload);
+}
+
 void setup(void) {
-  Serial.begin(9600); //温度表示確認用
+  Serial.begin(9600);
   
+  //温度センサー関係
   sensors.setResolution(SENSER_BIT);
   ledcSetup(LEDC_CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_8_BIT) ; // 8ビット精度で制御
   ledcAttachPin(HEAT_PIN, LEDC_CHANNEL_0) ;
   preTime = micros();
+
+  //Wifiに接続
+  setup_wifi();
+  //Brokerサーバーへのアクセス
+  mqttClient.setServer(mqttHost, 1883);
+  mqttClient.setCallback(callback);
 }
   
 void loop(void) {
@@ -44,6 +136,7 @@ void loop(void) {
   ledcWrite(LEDC_CHANNEL_0, pwm);
   Serial.print("pwm: ");
   Serial.print(pwm);
+  delay(10);
   sensors.requestTemperatures();              // 温度取得要求
   temp = sensors.getTempCByIndex(0);
 
@@ -66,6 +159,11 @@ void loop(void) {
     Serial.println(send_temp);
   }
 
+  temp_pub();
+  if ( ! mqttClient.connected() ) {
+    reconnect();
+  }
+  mqttClient.loop();
 }
 
 float p(){
